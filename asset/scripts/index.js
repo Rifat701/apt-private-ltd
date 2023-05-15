95529bee99c517b2d49112b440d2c0ae75296147
Akib Hossain <akibhossain737@gmail.com>
2023-05-15 02:02:38 -0700

    first commit

---
+++ asset/scripts/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".banner").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 3000
+  });
+});
